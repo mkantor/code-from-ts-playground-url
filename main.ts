b13a4ac0be6f6ff27b33a5164126bd95617b511a
Matt Kantor <the.matt.kantor@gmail.com>
2021-02-08 15:08:21 -0800

    Simple script to grab code from a TS Playground URL.

---
+++ main.ts
@@ -0,0 +1,18 @@
+import { decompressFromBase64 } from "lz-string"
+
+if (process.argv.length !== 3) {
+  throw new Error(
+    "This script expects a single command-line argument (the TypeScript Playground URL to decode).",
+  )
+}
+
+const url = process.argv[2]
+const code = decompressFromBase64(new URL(url).hash.replace(/^#code\//, ""))
+
+if (typeof code !== "string") {
+  throw new Error(
+    `Failed to decode URL. Is "${url}" a valid TypeScript Playground URL?`,
+  )
+}
+
+console.log(code)
