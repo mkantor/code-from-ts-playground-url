88b72bf7395b8d5eb3a0e17e908de00c2e6c36d2
Matt Kantor <the.matt.kantor@gmail.com>
2021-02-10 10:59:15 -0800

    Rename some variables.

--- main.ts
+++ main.ts
@@ -9,27 +9,27 @@ function codeFromUrl(url: URL): string | null {
 async function main(): Promise<void> {
   // Expected invocation is `node path/to/index.js $url`. If $url is "-" or
   // there are no arguments, read from STDIN.
-  let lines
+  let inputs
   if (
     process.argv.length < 3 ||
     (process.argv.length === 3 && process.argv[2] === "-")
   ) {
     process.stdin.setEncoding("utf8")
-    lines = readline.createInterface({ input: process.stdin })
+    inputs = readline.createInterface({ input: process.stdin })
   } else {
-    lines = Readable.from(process.argv.slice(2))
+    inputs = Readable.from(process.argv.slice(2))
   }
 
-  for await (const line of lines) {
-    const trimmedLine = line.trim()
-    if (trimmedLine.length === 0) {
+  for await (const input of inputs) {
+    const trimmedInput = input.trim()
+    if (trimmedInput.length === 0) {
       throw new Error("No URL was provided.")
     }
 
-    const code = codeFromUrl(new URL(trimmedLine))
+    const code = codeFromUrl(new URL(trimmedInput))
     if (typeof code !== "string" || code.length === 0) {
       throw new Error(
-        `Failed to decode URL. Is "${trimmedLine}" a valid TypeScript Playground URL?`,
+        `Failed to decode URL. Is "${trimmedInput}" a valid TypeScript Playground URL?`,
       )
     }
 
